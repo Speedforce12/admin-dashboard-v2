47bd273cd6eb6589b218ff5a48ec9389dab349d4
Speedforce12 <ovoneedee@gmail.com>
2022-11-12 22:44:46 -0400

    added backend layout and restructure app

---
+++ server/models/User.js
@@ -0,0 +1,22 @@
+import mongoose from "mongoose";
+
+const userSchema = mongoose.Schema(
+  {
+    firstName: { type: String, min: 2, max: 50, required: true },
+    lastName: { type: String, min: 2, max: 50, required: true },
+    email: {
+      type: String,
+      min: 5,
+      max: 50,
+      required: true,
+      unique: true,
+      trim: true,
+    },
+    password: { type: String, required: true },
+    photoURL: { type: String, default: "" },
+  },
+  { timestamps: true }
+);
+
+const User = mongoose.model("users", userSchema);
+export default User;
